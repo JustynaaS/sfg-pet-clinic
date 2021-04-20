54aabd05f03aabfd8a870fed8f9f547294025bb1
dzastinka <jslawska5@gmail.com>
2021-04-20 16:07:33 +0200

    add specialties to vets on startup

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,9 +3,11 @@ package guru.springframework.sfgpetclinic.bootstrap;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -17,15 +19,24 @@ public class DataLoader implements CommandLineRunner {
 private final OwnerService ownerService;
 private final VetService vetService;
 private final PetTypeService petTypeService;
+private final SpecialtyService specialtyService;
 
-public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
     this.petTypeService = petTypeService;
+    this.specialtyService = specialtyService;
 }
 
 @Override
 public void run(String... args) throws Exception {
+    int count = petTypeService.findAll().size();
+    if (count == 0) {
+        loadData();
+    }
+}
+
+private void loadData() {
     PetType dog = new PetType();
     dog.setName("Dog");
     PetType dogPetType = petTypeService.save(dog);
@@ -33,6 +44,18 @@ public void run(String... args) throws Exception {
     PetType cat = new PetType();
     dog.setName("Cat");
     PetType catPetType = petTypeService.save(cat);
+
+    Specialty radiology = new Specialty();
+    radiology.setDescription("Radiology");
+    Specialty savedRadilogy = specialtyService.save(radiology);
+
+    Specialty surgery = new Specialty();
+    radiology.setDescription("Surgery");
+    Specialty savedSurgery = specialtyService.save(surgery);
+
+    Specialty dentistry = new Specialty();
+    radiology.setDescription("Dentistry");
+    Specialty savedDentistry = specialtyService.save(dentistry);
 
     Owner owner1 = new Owner();
     owner1.setFirstName("Michael");
@@ -70,12 +93,14 @@ public void run(String... args) throws Exception {
     Vet vet1 = new Vet();
     vet1.setFirstName("Sam");
     vet1.setLastName("Axe");
+    vet1.getSpecialties().add(savedRadilogy);
 
     vetService.save(vet1);
 
     Vet vet2 = new Vet();
     vet2.setFirstName("Jessie");
     vet2.setLastName("Porter");
+    vet2.getSpecialties().add(savedSurgery);
 
     vetService.save(vet2);
     System.out.println("Loaded vets...");
