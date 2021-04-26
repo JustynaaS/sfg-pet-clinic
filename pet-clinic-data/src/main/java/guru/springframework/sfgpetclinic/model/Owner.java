5d41b1e91a4353ac51893510d808407b6ef14326
dzastinka <jslawska5@gmail.com>
2021-04-26 19:40:58 +0200

    update vet to use JPA

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,13 +3,14 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
+
+import static javax.persistence.CascadeType.ALL;
 
 @Getter
 @Setter
@@ -25,7 +26,7 @@ private String city;
 @Column(name = "telephone")
 private String telephone;
 
-@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+@OneToMany(cascade = ALL, mappedBy = "owner")
 private Set<Pet> pets = new HashSet<>();
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,11 +3,23 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+import static javax.persistence.FetchType.EAGER;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+
+@ManyToMany(fetch = EAGER)
+@JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 private Set<Specialty> specialties = new HashSet<>();
 }
